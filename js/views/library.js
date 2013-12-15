47a1b4e1de7cae0fc289567830451fae3f83d795
Christian <shinobiwulf@gmail.com>
2013-12-15 15:59:29 -0800

    add and delete records

--- js/views/library.js
+++ js/views/library.js
@@ -5,6 +5,7 @@ app.LibraryView = Backbone.View.extend({
 
 	initialize: function(initialBooks) {
 		this.collection = new app.Library(initialBooks);
+		this.listenTo(this.collection, 'add', this.renderBook);
 		this.render();
 	},
 
@@ -20,6 +21,26 @@ app.LibraryView = Backbone.View.extend({
 		var bookView = new app.BookView({
 			model: item
 		});
+		
 		this.$el.append(bookView.render().el);
+	},
+
+	events: {
+		'click #add' : 'addBook'
+	},
+
+	addBook: function(e) {
+		e.preventDefault();
+
+		var formData = {};
+
+		$('#addBook div').children('input').each(function(i, el) {
+			if($(el).val() != '') {
+				formData[el.id] = $(el).val();
+			}
+		});
+
+		this.collection.add(new app.Book(formData));
 	}
 });
+
