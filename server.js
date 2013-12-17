26448d623c0292b0f3b12f95605a03e63ee0865f
Christian <shinobiwulf@gmail.com>
2013-12-17 14:06:38 -0800

    added add feature to db

--- server.js
+++ server.js
@@ -80,3 +80,34 @@ app.post('/api/books', function(request, response) {
 
 	return response.send(book);
 });
+
+// Get a single book by id
+app.get( '/api/books/:id', function( request, response ) {
+	return BookModel.findById( request.params.id, function( err, book ) {
+		if( !err ) {
+			return response.send( book );
+		} else {
+			return console.log( err );
+		}
+	});
+});
+
+// Update a book
+app.put('/api/books/:id', function(request, response) {
+	console.log('Updating book ' + request.body.title);
+	return BookModel.findById(request.params.id, function(err, book) {
+		book.title = request.body.title;
+		book.author = request.body.author;
+		book.releaseDate = request.body.releaseDate;
+
+		return book.save(function(err) {
+			if(!err) {
+				console.log('book updated');
+			} else {
+				console.log(err);
+			}
+
+			return response.send(book);
+		});
+	});
+});
