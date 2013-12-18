dbc371be78406280b3e881a4f61ab7b4c6c7ca7e
Christian <shinobiwulf@gmail.com>
2013-12-18 11:37:29 -0800

    bug fixes

--- js/views/cookbook.js
+++ js/views/cookbook.js
@@ -40,7 +40,7 @@ app.CookbookView = Backbone.View.extend({
 		$('#addRecipe div').children('input').each(function(i, el) {
 			if($(el).val() != '') {
 				if(el.id === 'keywords') {
-					formData[el.id] = [];
+ 					formData[el.id] = [];
 					_.each($(el).val().split(''), function (keyword) {
 						formData[el.id].push({'keyword': keyword});
 					});
